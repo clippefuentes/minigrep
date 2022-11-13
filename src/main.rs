a02363e52b6113e0e8a1def3d84adcf91f065bee
Clyne Fuentes <clippefuentes@gmail.com>
2022-11-13 16:36:53 -0600

    can read file and display them

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::env;
+use std::fs;
+use std::process;
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+       println!("Problem with Parsing: {}", err);
+       process::exit(1); 
+    });
+    
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+    
+    // run(config);
+
+    if let Err(e) = run(config) {
+        println!("Application Error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("with Text: \n {}", contents);
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Must provide enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Ok(Config { query, filename })
+    }
 }
